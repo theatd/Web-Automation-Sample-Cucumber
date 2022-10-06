50bac00faa4b4cbb44f562e2fea10e0179f62d31
theatd <97729650+theatd@users.noreply.github.com>
2022-10-06 12:28:01 +0200

    Update ObjectRepository.java

--- src/main/java/models/common/ObjectRepository.java
+++ src/main/java/models/common/ObjectRepository.java
@@ -5,7 +5,10 @@ import pages.LandingPage;
 import pages.StorePage;
 
 public class ObjectRepository {
-    LandingPage landingPage = new LandingPage();
-    StorePage storePage = new StorePage();
-    BuyMacPage buyMacPage = new BuyMacPage();
+     LandingPage landingPage = new LandingPage();
+     StorePage storePage = new StorePage();
+     ElementPage elementPage = new ElementPage();
+	 BuyMacPage buyMacPage = new BuyMacPage();
+	 HomePage homePage = new HomePage();
+	 FormPage formPage = new FormPage();
 }
